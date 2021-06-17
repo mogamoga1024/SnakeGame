e8a027cd8b68acb30643b87650896e6e5f99a2cd
mogamoga <mogamoga.g1thu6@gmail.com>
2021-06-17 23:50:34 +0900

    体

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 
-let x = 100;
-let y = 100;
+let headX = 100;
+let headY = 100;
 const radius = 25;
-let degree = 0;
+let degree = 30;
 let oldDegree = null;
 const speed = 3;
 const rotationDegree = 2;
 const traceQueue = []
+
+let preHeadX, preHeadY;
 
 function setup() {
     const canvasWidth = windowWidth * 0.9;
@@ -16,45 +18,86 @@ function setup() {
 
     angleMode(DEGREES);
 
-    traceQueue.push({x: x, y: y});
+    // traceQueue.push({x: headX, y: headY});
+
+    preHeadX = headX, preHeadY = headY;
 }
 
 function draw() {
     clear();
 
-    // push();
-    // stroke(0, 200, 0, 70);
-    // strokeWeight(radius * 2);
-    // line(100, 100, width - 100, height - 100);
-    // pop();
-    
-
     if (keyIsPressed) {
         degreeChangeByKey();
-        if (oldDegree != degree) {
-            oldDegree = degree;
-            traceQueue.push({x: x, y: y});
-        }
+        // if (oldDegree != degree) {
+        //     oldDegree = degree;
+        //     traceQueue.push({x: headX, y: headY});
+        // }
     }
 
-    if (0 < x - radius && x + radius < width && 0 < y - radius && y + radius < height) {
-        x += speed * cos(degree);
-        y += speed * sin(degree);
+    if (0 < headX - radius && headX + radius < width && 0 < headY - radius && headY + radius < height) {
+        headX += speed * cos(degree);
+        headY += speed * sin(degree);
     }
     else {
         noLoop();
     }
 
-    push();
-    stroke(255, 0, 0);
-    strokeWeight(radius);
-    for (let i = 0; i < traceQueue.length; i++) {
-        point(traceQueue[i].x, traceQueue[i].y);
-    }
-    pop();
+    // push();
+    // stroke(255, 0, 0);
+    // strokeWeight(radius);
+    // for (let i = 0; i < traceQueue.length; i++) {
+    //     point(traceQueue[i].x, traceQueue[i].y);
+    // }
+    // pop();
 
     fill(0, 200, 0);
-    ellipse(x, y, radius * 2);
+    ellipse(headX, headY, radius * 2);
+
+    // todo 浮動小数点の誤差も考慮すべき ← いらんと思う。
+    if (preHeadX === headX) {
+        throw new Error("体の縦移動未実装");
+    }
+
+    const a = (headY - preHeadY) / (headX - preHeadX);
+    const b = headY - a * headX;
+    const c = headX;
+    const d = headY;
+    const r = radius * 2;
+    const A = pow(a, 2) + 1;
+    const B = a * (b - d) - c;
+    const C = pow(b - d, 2) + pow(c, 2) - pow(r, 2);
+
+    const D = sqrt(pow(B, 2) - A * C);
+
+    if (Number.isNaN(D)) {
+        return;
+    }
+
+    const tmpBodyX1 = (-B - D) / A;
+    const tmpBodyX2 = (-B + D) / A;
+
+    let bodyX;
+    let minX, maxX;
+    if (preHeadX < headX) {
+        minX = preHeadX;
+        maxX = headX;
+    }
+    else {
+        minX = headX;
+        maxX = preHeadX;
+    }
+    if (minX <= tmpBodyX1 && tmpBodyX1 <= maxX) {
+        bodyX = tmpBodyX1;
+    }
+    else if (minX <= tmpBodyX2 && tmpBodyX2 <= maxX) {
+        bodyX = tmpBodyX2;
+    }
+    else {
+        return;
+    }
+    const bodyY = a * bodyX + b;
+
+    ellipse(bodyX, bodyY, radius * 2);
 }
 
 function degreeChangeByKey() {
