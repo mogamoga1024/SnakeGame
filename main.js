95dbb88202a1dd2a21b432b6a72d712f7d9775e5
mogamoga <mogamoga.g1thu6@gmail.com>
2021-06-19 15:11:48 +0900

    縦移動

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let headX = 100;
 let headY = 100;
 const radius = 25;
-let degree = 30;
+let degree = 90;
 let oldDegree = null;
 const speed = 3;
 const rotationDegree = 2;
@@ -53,51 +53,73 @@ function draw() {
     fill(0, 200, 0);
     ellipse(headX, headY, radius * 2);
 
-    // todo 浮動小数点の誤差も考慮すべき ← いらんと思う。
-    // if (preHeadX === headX) {
-    //     throw new Error("体の縦移動未実装");
-    // }
+    let bodyX, bodyY;
 
-    // const a = (headY - preHeadY) / (headX - preHeadX);
-    // const b = headY - a * headX;
-    // const c = headX;
-    // const d = headY;
-    // const r = radius * 2;
-    // const A = pow(a, 2) + 1;
-    // const B = a * (b - d) - c;
-    // const C = pow(b - d, 2) + pow(c, 2) - pow(r, 2);
+    if (preHeadX === headX) {
+        const tmpBodyY1 = headY - radius * 2;
+        const tmpBodyY2 = headY + radius * 2;
 
-    // const D = sqrt(pow(B, 2) - A * C);
+        let minY, maxY;
+        if (preHeadY < headY) {
+            minY = preHeadY;
+            maxY = headY;
+        }
+        else {
+            minY = headY;
+            maxY = preHeadY;
+        }
+        if (minY <= tmpBodyY1 && tmpBodyY1 <= maxY) {
+            bodyY = tmpBodyY1;
+        }
+        else if (minY <= tmpBodyY2 && tmpBodyY2 <= maxY) {
+            bodyY = tmpBodyY2;
+        }
+        else {
+            return;
+        }
+        bodyX = headX;
+    }
+    else {
+        const a = (headY - preHeadY) / (headX - preHeadX);
+        const b = headY - a * headX;
+        const c = headX;
+        const d = headY;
+        const r = radius * 2;
+        const A = pow(a, 2) + 1;
+        const B = a * (b - d) - c;
+        const C = pow(b - d, 2) + pow(c, 2) - pow(r, 2);
 
-    // if (Number.isNaN(D)) {
-    //     return;
-    // }
+        const D = sqrt(pow(B, 2) - A * C);
 
-    // const tmpBodyX1 = (-B - D) / A;
-    // const tmpBodyX2 = (-B + D) / A;
+        if (Number.isNaN(D)) {
+            return;
+        }
 
-    // let bodyX;
-    // let minX, maxX;
-    // if (preHeadX < headX) {
-    //     minX = preHeadX;
-    //     maxX = headX;
-    // }
-    // else {
-    //     minX = headX;
-    //     maxX = preHeadX;
-    // }
-    // if (minX <= tmpBodyX1 && tmpBodyX1 <= maxX) {
-    //     bodyX = tmpBodyX1;
-    // }
-    // else if (minX <= tmpBodyX2 && tmpBodyX2 <= maxX) {
-    //     bodyX = tmpBodyX2;
-    // }
-    // else {
-    //     return;
-    // }
-    // const bodyY = a * bodyX + b;
+        const tmpBodyX1 = (-B - D) / A;
+        const tmpBodyX2 = (-B + D) / A;
 
-    // ellipse(bodyX, bodyY, radius * 2);
+        let minX, maxX;
+        if (preHeadX < headX) {
+            minX = preHeadX;
+            maxX = headX;
+        }
+        else {
+            minX = headX;
+            maxX = preHeadX;
+        }
+        if (minX <= tmpBodyX1 && tmpBodyX1 <= maxX) {
+            bodyX = tmpBodyX1;
+        }
+        else if (minX <= tmpBodyX2 && tmpBodyX2 <= maxX) {
+            bodyX = tmpBodyX2;
+        }
+        else {
+            return;
+        }
+        bodyY = a * bodyX + b;
+    }
+
+    ellipse(bodyX, bodyY, radius * 2);
 }
 
 function degreeChangeByKey() {
