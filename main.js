71b6340c6c0e4dacb68d90346815416051b64c96
mogamoga <mogamoga.g1thu6@gmail.com>
2021-06-19 16:41:12 +0900

    リファ

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 
-let headX = 100;
-let headY = 100;
+let headPotision = {x: 100, y: 100};
 const radius = 25;
 let degree = 90;
 let oldDegree = null;
 const speed = 3;
 const rotationDegree = 2;
 const traceQueue = []
-
-let preHeadX, preHeadY;
+let preHeadPotision;
 
 function setup() {
     const canvasWidth = windowWidth * 0.9;
@@ -18,9 +16,9 @@ function setup() {
 
     angleMode(DEGREES);
 
-    // traceQueue.push({x: headX, y: headY});
+    // traceQueue.push(headPotision);
 
-    preHeadX = headX, preHeadY = headY;
+    preHeadPotision = {x: headPotision.x, y: headPotision.y}
 }
 
 function draw() {
@@ -30,13 +28,13 @@ function draw() {
         degreeChangeByKey();
         // if (oldDegree != degree) {
         //     oldDegree = degree;
-        //     traceQueue.push({x: headX, y: headY});
+        //     traceQueue.push(headPotision);
         // }
     }
 
-    if (0 < headX - radius && headX + radius < width && 0 < headY - radius && headY + radius < height) {
-        headX += speed * cos(degree);
-        headY += speed * sin(degree);
+    if (0 < headPotision.x - radius && headPotision.x + radius < width && 0 < headPotision.y - radius && headPotision.y + radius < height) {
+        headPotision.x += speed * cos(degree);
+        headPotision.y += speed * sin(degree);
     }
     else {
         noLoop();
@@ -51,75 +49,13 @@ function draw() {
     // pop();
 
     fill(0, 200, 0);
-    ellipse(headX, headY, radius * 2);
+    ellipse(headPotision.x, headPotision.y, radius * 2);
 
-    let bodyX, bodyY;
+    const backBodyPosition = getBackBodyPosition(headPotision, headPotision, preHeadPotision);    
 
-    if (preHeadX === headX) {
-        const tmpBodyY1 = headY - radius * 2;
-        const tmpBodyY2 = headY + radius * 2;
-
-        let minY, maxY;
-        if (preHeadY < headY) {
-            minY = preHeadY;
-            maxY = headY;
-        }
-        else {
-            minY = headY;
-            maxY = preHeadY;
-        }
-        if (minY <= tmpBodyY1 && tmpBodyY1 <= maxY) {
-            bodyY = tmpBodyY1;
-        }
-        else if (minY <= tmpBodyY2 && tmpBodyY2 <= maxY) {
-            bodyY = tmpBodyY2;
-        }
-        else {
-            return;
-        }
-        bodyX = headX;
+    if (backBodyPosition != null) {
+        ellipse(backBodyPosition.x, backBodyPosition.y, radius * 2);
     }
-    else {
-        const a = (headY - preHeadY) / (headX - preHeadX);
-        const b = headY - a * headX;
-        const c = headX;
-        const d = headY;
-        const r = radius * 2;
-        const A = pow(a, 2) + 1;
-        const B = a * (b - d) - c;
-        const C = pow(b - d, 2) + pow(c, 2) - pow(r, 2);
-
-        const D = sqrt(pow(B, 2) - A * C);
-
-        if (Number.isNaN(D)) {
-            return;
-        }
-
-        const tmpBodyX1 = (-B - D) / A;
-        const tmpBodyX2 = (-B + D) / A;
-
-        let minX, maxX;
-        if (preHeadX < headX) {
-            minX = preHeadX;
-            maxX = headX;
-        }
-        else {
-            minX = headX;
-            maxX = preHeadX;
-        }
-        if (minX <= tmpBodyX1 && tmpBodyX1 <= maxX) {
-            bodyX = tmpBodyX1;
-        }
-        else if (minX <= tmpBodyX2 && tmpBodyX2 <= maxX) {
-            bodyX = tmpBodyX2;
-        }
-        else {
-            return;
-        }
-        bodyY = a * bodyX + b;
-    }
-
-    ellipse(bodyX, bodyY, radius * 2);
 }
 
 function degreeChangeByKey() {
@@ -176,4 +112,72 @@ function minusRotate(degree) {
     return (degree - rotationDegree + 360) % 360;
 }
 
+function getBackBodyPosition(basePosition, locusPoint1, locusPoint2) {
+    let backBodyX, backBodyY;
 
+    if (locusPoint2.x === locusPoint1.x) {
+        const tmpBodyY1 = basePosition.y - radius * 2;
+        const tmpBodyY2 = basePosition.y + radius * 2;
+
+        let minY, maxY;
+        if (locusPoint2.y < locusPoint1.y) {
+            minY = locusPoint2.y;
+            maxY = locusPoint1.y;
+        }
+        else {
+            minY = locusPoint1.y;
+            maxY = locusPoint2.y;
+        }
+        if (minY <= tmpBodyY1 && tmpBodyY1 <= maxY) {
+            backBodyY = tmpBodyY1;
+        }
+        else if (minY <= tmpBodyY2 && tmpBodyY2 <= maxY) {
+            backBodyY = tmpBodyY2;
+        }
+        else {
+            return null;
+        }
+        backBodyX = locusPoint1.x;
+    }
+    else {
+        const a = (locusPoint1.y - locusPoint2.y) / (locusPoint1.x - locusPoint2.x);
+        const b = locusPoint1.y - a * locusPoint1.x;
+        const c = basePosition.x;
+        const d = basePosition.y;
+        const r = radius * 2;
+        const A = pow(a, 2) + 1;
+        const B = a * (b - d) - c;
+        const C = pow(b - d, 2) + pow(c, 2) - pow(r, 2);
+
+        const D = sqrt(pow(B, 2) - A * C);
+
+        if (Number.isNaN(D)) {
+            return null;
+        }
+
+        const tmpBodyX1 = (-B - D) / A;
+        const tmpBodyX2 = (-B + D) / A;
+
+        let minX, maxX;
+        if (locusPoint2.x < locusPoint1.x) {
+            minX = locusPoint2.x;
+            maxX = locusPoint1.x;
+        }
+        else {
+            minX = locusPoint1.x;
+            maxX = locusPoint2.x;
+        }
+        if (minX <= tmpBodyX1 && tmpBodyX1 <= maxX) {
+            backBodyX = tmpBodyX1;
+        }
+        else if (minX <= tmpBodyX2 && tmpBodyX2 <= maxX) {
+            backBodyX = tmpBodyX2;
+        }
+        else {
+            return null;
+        }
+        backBodyY = a * backBodyX + b;
+    }
+
+    return {x: backBodyX, y: backBodyY};
+}
