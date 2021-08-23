c41e1fb7d7e705c46cad82d58f2aa4ad5ee0b367
mogamoga <mogamoga.g1thu6@gmail.com>
2021-08-24 00:53:18 +0900

    SceneManager

--- js/main.js
+++ js/main.js
@@ -6,4 +6,4 @@ const $svg = $("svg");
 $svg.css("top", ($window.height() - $svg.height()) / 2);
 $svg.css("left", ($window.width() - $svg.width()) / 2);
 
-
+SceneManager.start(new Scene());
--- js/scene/sceneManager.js
+++ js/scene/sceneManager.js
@@ -1,4 +1,4 @@
-
+/*
 const SceneManager = (function() {
     let currentScene = new Scene();
     draw = function() {
@@ -17,3 +17,25 @@ const SceneManager = (function() {
         }
     }
 })();
+*/
+
+const SceneManager = (function() {
+    let timer = null;
+
+    return {
+        start: function(scene, _shouldUpdate) {
+            const shouldUpdate = (_shouldUpdate === undefined) ? true : _shouldUpdate;
+            clearInterval(timer);
+            scene.start();
+            if (shouldUpdate) {
+                timer = setInterval(function() {
+                    console.log("hello, world!");
+                    scene.update();
+                }, 1000 / 60);
+            }
+        }
+    };
+})();
+
+
+
