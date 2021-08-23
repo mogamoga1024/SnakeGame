444b972605e34819299e3a3afb69bfda5bf7802f
mogamoga <mogamoga.g1thu6@gmail.com>
2021-08-24 00:12:43 +0900

    土台

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
 
-let feedCanvas, snakeCanvas;
-let canvasWidth, canvasHeight;
 
-function setup() {
-    const canvasWidth = windowWidth * 0.9;
-    const canvasHeight = windowHeight * 0.9;
-    const canvas = createCanvas(canvasWidth, canvasHeight);
-    canvas.position((windowWidth - canvasWidth) / 2, (windowHeight - canvasHeight) / 2);
-    
-    feedCanvas = createGraphics(canvasWidth, canvasHeight);
-    snakeCanvas = createGraphics(canvasWidth, canvasHeight);
+const $window = $(window);
+const $svg = $("svg");
 
-    SceneManager.start(new GmaeStartScene());
+$svg.css("top", ($window.height() - $svg.height()) / 2);
+$svg.css("left", ($window.width() - $svg.width()) / 2);
 
-    frameRate(60);
-}
+
